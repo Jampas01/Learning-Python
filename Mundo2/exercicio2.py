5f06db254d58459bb77e8a39c9225f9f1ba8db0b
Jampas01 <jpsg08042000@gmail.com>
2025-11-16 12:20:26 -0300

    correcao

--- Mundo2/exercicio2.py
+++ Mundo2/exercicio2.py
@@ -14,3 +14,6 @@ elif opcao == 2:
     print('O número {} em OCTAL é: {}'.format(num, oct(num)[2:]))
 elif opcao == 3:
     print('O número {} em HEXADECIMAL é: {}'.format(num, hex(num)[2:]))
+else:
+    print('Opção inválida! Tente Novamente.')
+    
