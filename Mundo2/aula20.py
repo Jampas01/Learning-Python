b56fb8a21e9b91a7e1e58a5e91d33f2e9585fe9f
Jampas01 <jpsg08042000@gmail.com>
2025-12-04 21:03:19 -0300

    Merge branch 'master' of https://github.com/Jampas01/Learning-Python

--- Mundo2/aula20.py
+++ Mundo2/aula20.py
@@ -1,5 +1,4 @@
-#Faça um programa que leia um numero qualquer e moste o seu fatorial;
-# ex: 5! = 5 x 4 x 3 x 2 x 1 = 120
+#Faça um programa que leia um numero qualquer e moste o seu fatorial;ex: 5! = 5 x 4 x 3 x 2 x 1 = 120
 '''from math import factorial
 
 n = int(input('Digite um número para calcular seu fatorial: '))
