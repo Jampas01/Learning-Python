750498485a30e3d75d342e0ef641c4b9fdbfb38c
Jampas01 <jpsg08042000@gmail.com>
2026-01-19 20:00:05 -0300

    revisao12

---
+++ GeradorCurriculo/main.py
@@ -0,0 +1,13 @@
+print('-=' * 25)
+print('GERADOR DE CURRÍCULUM')
+print('-=' * 25)
+
+#RECOLHIMENTO DE DADOS
+r = 'S'
+while r == 'S':
+    nome = str(input('Digite seu nome: '))
+    idade = int(input('Digite sua idade: '))
+    datanasc = int(input('Digite sua data de nascimento: '))
+    
+
+      
