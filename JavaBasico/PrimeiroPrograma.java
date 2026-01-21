a8428c7f3960ae7986c5891c2495c133f24feaf8
Jampas01 <jpsg08042000@gmail.com>
2026-01-21 20:54:04 -0300

    OlaMundoJV

---
+++ JavaBasico/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+// Aula 01 de Java (coffee kkkkkkkk)
+
+// JVM == Java Virtual Machine
+
+// procurarei mais...
+
+
+public class PrimeiroPrograma {
+    public static void main(String[]args) {
+        System.out.println("Olá Mundo!");
+        System.out.println("Vamos aprender JAVA?");
+    }
+}
--- JavaBasico/aula1.java
+++
@@ -1,5 +0,0 @@
-// Aula 01 de Java (coffee kkkkkkkk)
-
-// JVM == Java Virtual Machine
-
-// procurarei mais...
